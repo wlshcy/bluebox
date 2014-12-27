0f822c4fcb21920aab536707fd631b52ead29ab2
niuminguo <minguon@jumei.com>
2014-12-27 05:15:10 -0500

    modified

--- warehouse/views.py
+++ warehouse/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response as response
 
 def index(request):
-    return render_to_response('warehouse.html')
+    return response('warehouse.html')
