8c1a6db551e9e3a9044d9d1fc4c98743f83a0221
mgniu <mgniu@dataman-inc.com>
2016-08-20 13:37:20 +0800

    fixed order url

--- orders/urls.py
+++ orders/urls.py
@@ -3,5 +3,9 @@ from django.conf.urls import patterns,url
 from orders import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index'),
+    url(r'^search$', views.index, name='index'),
+    url(r'^all$', views.index, name='index'),
+    url(r'^unpay$', views.index, name='index'),
+    url(r'^delivering$', views.index, name='index'),
+    url(r'^finished$', views.index, name='index'),
 )
